ef610e5002b1839d5b637c7c1d74c7fb30218a25
cheny0.0@qq.com <cheny0.0@qq.com>
2019-03-05 17:34:38 +0800

    vue骨架屏测试

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+const app = new Vue({
+  components: {
+    App,
+  },
   render: h => h(App),
-}).$mount('#app')
+});
+
+window.mountApp = () => {
+  app.$mount('#app');
+};
+
+if (process.env.NODE_ENV === 'production') {
+  if (window.STYLE_READY) {
+    window.mountApp();
+  }
+} else {
+  window.mountApp();
+}
